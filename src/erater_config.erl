efde7c14a871e3c684c696f0ebb64d133b2ed92f
Danil Zagoskin <stolen@yandex-team.ru>
2014-06-20 20:28:41 +0400

    handle shards option properly

--- src/erater_config.erl
+++ src/erater_config.erl
@@ -1,5 +1,6 @@
 -module(erater_config).
--export([validate/1, clean/1, rps/1, capacity/1, die_after/1]).
+-export([validate/1, clean/1]).
+-export([rps/1, capacity/1, die_after/1, shards/1]).
 
 
 validate(Config) when is_list(Config) ->
@@ -7,13 +8,16 @@ validate(Config) when is_list(Config) ->
     true = is_integer(capacity(Config)),
     DieAfter = die_after(Config),
     true = is_integer(DieAfter) orelse (DieAfter == infinity),
+    Shards = shards(Config),
+    true = is_integer(Shards) orelse (Shards == undefined),
     ok.
 
 clean(Config) ->
     [
-        {rps, erater_config:rps(Config)},
-        {capacity, erater_config:capacity(Config)},
-        {die_after, erater_config:die_after(Config)}
+        {rps, rps(Config)},
+        {capacity, capacity(Config)},
+        {die_after, die_after(Config)},
+        {shards, shards(Config)}
         ].
 
 
@@ -24,4 +28,7 @@ capacity(Config) ->
     proplists:get_value(capacity, Config, 1).
 
 die_after(Config) ->
-    proplists:get_value(die_after, Config).
+    proplists:get_value(die_after, Config, 300000).
+
+shards(Config) ->
+    proplists:get_value(shards, Config).
--- src/erater_shard.erl
+++ src/erater_shard.erl
@@ -10,7 +10,7 @@
 -export([handle_call/3, handle_cast/2, handle_info/2]).
 
 start_link(Group, Config) ->
-    case proplists:get_value(shards, Config) of
+    case erater_config:shards(Config) of
         undefined ->
             ignore;
         Shards when is_integer(Shards) ->
